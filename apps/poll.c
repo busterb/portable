4b0d077778e3ac1b8684da3c3593c7b0fc6c2571
Brent Cook <busterb@gmail.com>
2014-11-23 08:41:35 -0600

    poll(2) emulation updates
     * use nfds_t directly for iterating the fds.
     * ensure this poll emulation is only build on Windows hosts.
     * add WSAGetLastError -> errno mappings
     * handle POLLHUP and the OOB data cases for revents
        (this is currently unused in openssl(1))
     * handle sparse arrays of fds correctly
     * KNF style updates

--- apps/poll.c
+++ apps/poll.c
@@ -1,7 +1,14 @@
 /*
- * poll(2) emulation for Windows
  * Public domain
- * from Dongsheng Song <dongsheng.song@gmail.com>
+ *
+ * poll(2) emulation for Windows
+ *
+ * This emulates just-enough poll functionality on Windows to work in the
+ * context of the openssl(1) program. This is not a replacement for
+ * POSIX.1-2001 poll(2).
+ *
+ * Dongsheng Song <dongsheng.song@gmail.com>
+ * Brent Cook <bcook@openbsd.org>
  */
 
 #include <ws2tcpip.h>
@@ -9,27 +16,106 @@
 #include <errno.h>
 #include <poll.h>
 
-static int compute_revents(int fd, short events, fd_set *rfds, fd_set *wfds,
-		fd_set *efds)
+static int
+conn_is_closed(int fd)
 {
-	short rc = 0;
+	char buf[1];
+	int ret = recv(fd, buf, 1, MSG_PEEK);
+	if (ret == -1) {
+		switch (WSAGetLastError()) {
+		case WSAECONNABORTED:
+		case WSAECONNRESET:
+		case WSAENETRESET:
+		case WSAESHUTDOWN:
+			return 1;
+		}
+	}
+	return 0;
+}
 
-	if (FD_ISSET(fd, efds))
-		return POLLERR;
+static int
+conn_has_oob_data(int fd)
+{
+	char buf[1];
+	return (recv(fd, buf, 1, MSG_PEEK | MSG_OOB) == 1);
+}
 
-	if ((events & (POLLIN | POLLRDNORM | POLLRDBAND)) && FD_ISSET(fd, rfds))
-		rc |= POLLIN;
+static int
+compute_revents(int fd, short events, fd_set *rfds, fd_set *wfds, fd_set *efds)
+{
+	int rc = 0;
 
-	if ((events & (POLLOUT | POLLWRNORM | POLLWRBAND)) && FD_ISSET(fd, wfds))
+	if ((events & (POLLIN | POLLRDNORM | POLLRDBAND)) &&
+			FD_ISSET(fd, rfds)) {
+		if (conn_is_closed(fd))
+			rc |= POLLHUP;
+		else
+			rc |= POLLIN | POLLRDNORM;
+	}
+
+	if ((events & (POLLOUT | POLLWRNORM | POLLWRBAND)) &&
+			FD_ISSET(fd, wfds))
 		rc |= POLLOUT;
+
+	if (FD_ISSET(fd, efds)) {
+		if (conn_is_closed(fd))
+			rc |= POLLHUP;
+		else if (conn_has_oob_data(fd))
+			rc |= POLLRDBAND | POLLPRI;
+	}
 
 	return rc;
 }
 
-/* Just select(2) wrapper, ignored unsupported flags. */
-int poll(struct pollfd *pfds, nfds_t nfds, int timeout)
+static int
+wsa_select_errno(int err)
 {
-	int i, rc;
+	switch (err) {
+	case WSAEINTR:
+	case WSAEINPROGRESS:
+		errno = EINTR;
+		break;
+	case WSAEFAULT:
+		/*
+		 * Windows uses WSAEFAULT for both resource
+		 * allocation failures and arguments not being
+		 * contained in the user's address space. So,
+		 * we have to choose EFAULT or ENOMEM.
+		 */
+		errno = EFAULT;
+		break;
+	case WSAEINVAL:
+		errno = EINVAL;
+		break;
+	case WSANOTINITIALISED:
+		errno = EPERM;
+		break;
+	case WSAENETDOWN:
+		errno = ENOMEM;
+		break;
+	case WSAENOTSOCK:
+		/*
+		 * poll(2) obviously does not normally set
+		 * ENOTSOCK, the only fix would be to replace
+		 * select with something like
+		 * WaitForMultipleObjects. But the original
+		 * select(2) uses in openssl(1) would have
+		 * already been broken already if they used
+		 * file descriptors with select.
+		 */
+		errno = ENOTSOCK;
+		break;
+	}
+	return -1;
+}
+
+
+/* Just select(2) wrapper, ignored unsupported flags. */
+int
+poll(struct pollfd *pfds, nfds_t nfds, int timeout)
+{
+	nfds_t i;
+	int rc;
 	fd_set rfds, wfds, efds;
 	struct timeval tv;
 	struct timeval *ptv;
@@ -47,15 +133,15 @@ int poll(struct pollfd *pfds, nfds_t nfds, int timeout)
 		ptv->tv_usec = (timeout % 1000) * 1000;
 	}
 
-	FD_ZERO (&rfds);
-	FD_ZERO (&wfds);
-	FD_ZERO (&efds);
+	FD_ZERO(&rfds);
+	FD_ZERO(&wfds);
+	FD_ZERO(&efds);
 
-	for (i = 0; i < (int) nfds; i++) {
+	for (i = 0; i < nfds; i++) {
 		if (pfds[i].fd < 0)
 			continue;
 
-		FD_SET (pfds[i].fd, &efds);
+		FD_SET(pfds[i].fd, &efds);
 
 		if (pfds[i].events & (POLLIN | POLLRDNORM | POLLRDBAND))
 			FD_SET (pfds[i].fd, &rfds);
@@ -64,21 +150,19 @@ int poll(struct pollfd *pfds, nfds_t nfds, int timeout)
 			FD_SET (pfds[i].fd, &wfds);
 	}
 
-	/* Winsock ignore the first parameter. */
-	rc = select (0, &rfds, &wfds, &efds, ptv);
-	if (rc < 0) {
-		errno  = WSAGetLastError();
-		return rc;
-	}
+	/* Winsock ignores the first parameter. */
+	rc = select(0, &rfds, &wfds, &efds, ptv);
+	if (rc == SOCKET_ERROR)
+		return wsa_select_errno(WSAGetLastError());
 
 	rc = 0;
-	for (i = 0; i < (int) nfds; i++) {
+	for (i = 0; i < nfds; i++) {
 		pfds[i].revents = 0;
 
 		if (pfds[i].fd < 0)
 			continue;
 
-		pfds[i].revents = compute_revents (pfds[i].fd, pfds[i].events,
+		pfds[i].revents = compute_revents(pfds[i].fd, pfds[i].events,
 			&rfds, &wfds, &efds);
 
 		if (pfds[i].revents)
